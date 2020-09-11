cae549ed6d97a1312e6618899463ddfa41cf4923
Jorge Reyes <reyes.jorge@correounivalle.edu.co>
2020-09-10 22:52:50 -0500

    first commit firebase

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAREHkFiBXpo5vvbmmK4VX4yS0MAJ8H7oE",
+    authDomain: "covidapp-ca50f.firebaseapp.com",
+    databaseURL: "https://covidapp-ca50f.firebaseio.com",
+    projectId: "covidapp-ca50f",
+    storageBucket: "covidapp-ca50f.appspot.com",
+    messagingSenderId: "316438823841",
+    appId: "1:316438823841:web:15d318240d23e9bca781e0",
+    measurementId: "G-ZP9342MD45"
+  };
