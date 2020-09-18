2f6d1d5cdc8a14384f270e6912af16cddcd27e98
Jorge Reyes <reyes.jorge@correounivalle.edu.co>
2020-09-17 21:08:45 -0500

    CRUD inicial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAREHkFiBXpo5vvbmmK4VX4yS0MAJ8H7oE",
+    authDomain: "covidapp-ca50f.firebaseapp.com",
+    databaseURL: "https://covidapp-ca50f.firebaseio.com",
+    projectId: "covidapp-ca50f",
+    storageBucket: "covidapp-ca50f.appspot.com",
+    messagingSenderId: "316438823841",
+    appId: "1:316438823841:web:15d318240d23e9bca781e0",
+    measurementId: "G-ZP9342MD45"
+  }
 };
 
 /*
